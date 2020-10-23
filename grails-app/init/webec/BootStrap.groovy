beea2857ad2b8e4bb2b9d868de9d046c1028e277
Dierk Koenig <dierk.koenig@canoo.com>
2020-10-23 14:44:24 +0200

    week 6: bootstrapping rooms

--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -3,6 +3,11 @@ package webec
 class BootStrap {
 
     def init = { servletContext ->
+
+        for(int i=1; i<10; i++) {
+            new Room(description: "room"+i, capacity: 10).save()
+        }
+
     }
     def destroy = {
     }
