5c1146975fa8908e44961310def4619e3da59801
Dierk Koenig <dierk.koenig@canoo.com>
2020-10-30 13:03:05 +0100

    timeslots and date added to booking, Bootstrap enhanced

--- grails-app/domain/webec/Booking.groovy
+++ grails-app/domain/webec/Booking.groovy
@@ -3,8 +3,17 @@ package webec
 class Booking {     // relation, many-to-many
     Room   room     // many-to-one
     Person booker   // many-to-one
+    Date   date
+    String timeslot
+
+    public static final String AM  = "08:15 - 11:00"
+    public static final String PM1 = "12:15 - 15:00"
+    public static final String PM2 = "15:15 - 18:00"
+    public static final List<String> TIME_SLOTS = [AM, PM1, PM2]
 
     static constraints = {
+        date      nullable: false
+        timeslot  nullable: false, inList: TIME_SLOTS
         room      nullable: false // is the default but better be explicit
         booker    nullable: false
     }
--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -1,8 +1,27 @@
 package webec
 
+import org.codehaus.groovy.runtime.DateGroovyMethods
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        Person koenig = new Person(firstName: "Dierk",  lastName: "König").save()
+        Person holz   = new Person(firstName: "Dieter", lastName: "Holz").save()
+
+        Room aula     = new Room(description: "Aula",  capacity: 60).save()
+        Room seminar  = new Room(description: "1.311", capacity: 40).save()
+
+        Date today = new Date()
+        Date tomorrow = today
+        use(DateGroovyMethods) {
+            today.clearTime()
+            tomorrow = today + 1
+        } // small Groovy trick ;-)
+
+        new Booking(room: aula,    booker: koenig, date: today,    timeslot: Booking.AM).save()
+        new Booking(room: aula,    booker: koenig, date: tomorrow, timeslot: Booking.PM1).save()
+        new Booking(room: seminar, booker: holz,   date: today,    timeslot: Booking.PM1).save()
 
         for(int i=1; i<10; i++) {
             new Room(description: "room"+i, capacity: 10).save()
